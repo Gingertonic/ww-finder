7d6e19288ad57f80a44fe93c0666c4e0292689b2
Gingertonic <bethmschofield@gmail.com>
2020-02-14 09:36:35 +0000

    stub building class

--- lib/ww_finder/building.rb
+++ lib/ww_finder/building.rb
@@ -1,3 +1,21 @@
 class WWFinder::Building 
+    attr_accessor :name, :address, :url
+
+    @@all = []
+
+    def initialize(name, address, url)
+        @name = name 
+        @address = address 
+        @url = url
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    private 
+    def save 
+        @@all << self 
+    end 
  
 end
