a566f9f0b3e8ec8db0e9173bb310ff1c2045ee03
Gingertonic <bethmschofield@gmail.com>
2020-02-14 09:31:31 +0000

    basic cli loop

--- lib/ww_finder/cli.rb
+++ lib/ww_finder/cli.rb
@@ -1,9 +1,33 @@
 class WWFinder::CLI 
+    attr_accessor :input
+
     def run 
         welcome
+        app_loop
+        goodbye
     end
 
     def welcome 
-        puts "Hai"
+        puts "Hai! Hit enter to continue"
+    end
+
+    def instructions 
+        puts "Enter the option number to see more details"
+    end
+
+    def app_loop 
+        while input != "exit" do
+            @input = gets.strip
+            instructions
+            show_countries
+        end
+    end
+
+    def show_countries
+        puts "Here lie countries"
+    end
+
+    def goodbye 
+        puts "Happy working! Don't forget to take a break!"
     end
 end
