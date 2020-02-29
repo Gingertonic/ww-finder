b6f69d1835ee86ffaaeb89f8cc05e1547797758e
Gingertonic <bethmschofield@gmail.com>
2020-02-29 17:30:36 +0000

    improve flow and add color

--- lib/ww_finder/cli.rb
+++ lib/ww_finder/cli.rb
@@ -3,65 +3,68 @@ class WWFinder::CLI
 
     def run 
         welcome
-        app_loop
-        goodbye
+        print_buildings_list
+        get_building_selection
     end
 
     def welcome 
-        puts "Hai! Hit enter to continue"
-        gets
+        puts "\nHai there! Are you looking for a place to work in London today?".light_white.on_magenta.bold
+        puts "Hit enter to continue".green
+        get_user_selection
     end
 
-
-    def instructions 
-        puts "\nEnter the option number to see more details"
-    end
-
-    def app_loop 
-        while input != "exit" do
-            show_buildings
-        end
-    end
-
-    def show_buildings
+    def print_buildings_list
         WWFinder::Building.all.each.with_index(1) do | building, idx |
             puts "#{idx}: #{building.address}"
         end
+        get_building_selection
+    end
+
+    def instructions 
+        puts "\nEnter the option number to see more details".green
+    end
+
+    def get_building_selection
+        instructions
         get_user_selection
-        show_building if valid_input(WWFinder::Building.all)
+        valid_input(WWFinder::Building.all) ? show_building : error
     end
 
     def show_building
         building = WWFinder::Building.all[user_num_input]
         building.prepare_details
-        puts "\n#{building.name}".colorize(:magenta)
+        puts "\n#{building.name}".light_white.on_magenta.bold
         puts building.info
-        puts building.address.colorize(:light_magenta)
+        puts building.address.light_white.on_light_magenta
         what_next
     end
 
     def valid_input(data)
-        user_num_input > 0 && user_num_input <= data.length
+        user_num_input >= 0 && user_num_input <= data.length - 1
     end
 
     def error 
-        puts "Oops, that's not a valid option"
+        puts "\nOops, that's not a valid option".light_white.on_red.bold
+        get_building_selection
     end
 
     def what_next
-        puts "\nIf you are done, type 'exit', otherwise hit enter to continue\n"
+        puts "\nIf you are done, type 'exit', otherwise hit enter to continue".green
         get_user_selection
+        print_buildings_list
     end 
 
     def get_user_selection
         @input = gets.strip
+        goodbye if @input == "exit"
     end
 
     def user_num_input
-        input.to_i - 1
+        input.to_i - 1 
     end
 
     def goodbye 
-        puts "Happy working! Don't forget to take a break!"
+        puts "\nHappy working! Don't forget to take a break!\n".light_white.on_magenta.bold
+        exit
     end
 end
